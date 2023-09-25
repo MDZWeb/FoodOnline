0d0719f9d877e24edca27a7e0b408bc64597d91d
MDZ WEB <dawoodzahid127@gmail.com>
2023-09-25 16:22:09 +0500

    Changes1.1

--- FoodOnline/settings.py
+++ FoodOnline/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f*tprsx4$h(lff=%0l5=7=kr+z($xv8kj*vctq1+&&2ra%pn5w'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -76,11 +77,11 @@ WSGI_APPLICATION = 'FoodOnline.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'foodonlinedb',
-        'HOST': 'localhost',
-        'USER': 'postgres',
-        'PASSWORD': '08199322da',
-        'PORT': '5432',
+        'NAME': config('NAME'),
+        'HOST': config('HOST'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'PORT': config('PORT'),
     }
 }
 
